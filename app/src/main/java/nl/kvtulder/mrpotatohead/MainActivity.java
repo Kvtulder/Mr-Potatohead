5f8da623422cc421c5468ad3de993e263c18ea6e
kasper <kvtulder@gmail.com>
2018-02-09 00:53:04 +0100

    App now saves instance and restores image visibilities

--- app/src/main/java/nl/kvtulder/mrpotatohead/MainActivity.java
+++ app/src/main/java/nl/kvtulder/mrpotatohead/MainActivity.java
@@ -1,5 +1,6 @@
 package nl.kvtulder.mrpotatohead;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,61 +8,71 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Check if there is a saved instance and if possible, restore the image visibilities
+        if (savedInstanceState != null) {
+
+            Log.d("potato", "onCreate: saved instance found,restoring image visibilities");
+            HashMap<Integer, Integer> imageVisibilities =
+                    (HashMap<Integer, Integer>) savedInstanceState.getSerializable("imageVisibilities");
+
+            for (int imageID : imageVisibilities.keySet()) {
+                // restore the visibilities
+                int visibility = imageVisibilities.get(imageID);
+                ImageView image = (ImageView) findViewById(imageID);
+                image.setVisibility(visibility);
+            }
+        }
     }
 
     public void checkClicked(View v)
     {
         CheckBox checkbox = (CheckBox) v;
-        String text = ((CheckBox) v).getText().toString();
         int ID;
 
-        switch (checkbox.getId())
-        {
-            case R.id.checkEars:
-                ID = R.id.imageEars;
+        int[] imagesIDs = {R.id.imageArms,R.id.imageEars,R.id.imageEyebrows,
+                R.id.imageEyes, R.id.imageGlasses,R.id.imageHat,
+                R.id.imageMouth,R.id.imageMustache,R.id.imageNose,R.id.imageShoes};
+
+        int[] checkboxIDs = {R.id.checkArms,R.id.checkEars,R.id.checkEyebrows,
+                R.id.checkEyes, R.id.checkGlasses,R.id.checkHat,
+                R.id.checkMouth,R.id.checkMustache,R.id.checkNose,R.id.checkShoes};
+
+        for(int i = 0 ;i < checkboxIDs.length;i++) {
+            if (checkboxIDs[i] == checkbox.getId()) {
+                String text = ((CheckBox) v).getText().toString();
+                ImageView image = (ImageView) findViewById(imagesIDs[i]);
+                boolean checked = checkbox.isChecked();
+                image.setVisibility(checked ? View.VISIBLE : View.INVISIBLE);
+                Log.d("potato", "checkClicked " + checkbox.getId());
                 break;
-            case R.id.checkEyes:
-                ID = R.id.imageEyes;
-                break;
-            case R.id.checkEyebrows:
-                ID = R.id.imageEyebrows;
-                break;
-            case R.id.checkGlasses:
-                ID = R.id.imageGlasses;
-                break;
-            case R.id.checkHat:
-                ID = R.id.imageHat;
-                break;
-            case R.id.checkMouth:
-                ID = R.id.imageMouth;
-                break;
-            case R.id.checkMustache:
-                ID = R.id.imageMustache;
-                break;
-            case R.id.checkNose:
-                ID = R.id.imageNose;
-                break;
-            case R.id.checkShoes:
-                ID = R.id.imageShoes;
-                break;
-            case R.id.checkArms:
-                ID = R.id.imageArms ;
-                break;
-            // something unexpected happened: there is a unknown checkbox. Stop the callback
-            default:
-                return;
+            }
         }
+    }
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
 
-        ImageView image = (ImageView) findViewById(ID);
-        boolean checked = checkbox.isChecked();
-        image.setVisibility(checked ? View.VISIBLE : View.INVISIBLE);
-        Log.d("potato","checkClicked " + checkbox.getId());
+        int[] imagesID = {R.id.imageArms,R.id.imageEars,R.id.imageEyebrows,
+                R.id.imageEyes, R.id.imageGlasses,R.id.imageHat,
+                R.id.imageMouth,R.id.imageMustache,R.id.imageNose,R.id.imageShoes};
 
+        // Create a hashmap storing all the image visibilities
+        HashMap<Integer,Integer> imageVisibilities = new HashMap<Integer,Integer>();
+        for(int imageID : imagesID)
+            imageVisibilities.put(imageID,findViewById(imageID).getVisibility());
+
+        // Hashmap implements Serializable, so we can use putSerializable
+        // https://stackoverflow.com/questions/3422388/android-get-value-from-hashmap
+        outState.putSerializable("imageVisibilities", imageVisibilities);
     }
 }
+
+
